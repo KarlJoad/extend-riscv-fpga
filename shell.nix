090002157b3ef4f13e9b448efee9c3711a18ae2e
Karl Hallsby <karl@hallsby.com>
2022-06-13 23:06:54 -0500

    Add shell.nix for Chipyard/verilator dev

---
+++ shell.nix
@@ -0,0 +1,43 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs.lib;
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    verilog   # Icarus Verilog compiler
+    svlint    # SystemVerilog Linter
+    svls      # SystemVerilog Language Server
+    verilator # (System)Verilog Simulator/Compiler
+
+    autoconf automake
+    sbt
+
+    # ChipYard Dependencies
+    coreutils moreutils binutils
+    bison
+    flex
+    gmp mpfr libmpc zlib
+    vim git jdk
+    texinfo gengetopt
+    expat libusb ncurses cmake
+    perl perlPackages.ExtUtilsMakeMaker
+    # Deps for poky
+    python3 patch diffstat texinfo subversion chrpath git wget
+    # Deps for qemu
+    gtk3 pkg-config
+    # Deps for firemarshall
+    python3 python3Packages.pip
+    rsync libguestfs texinfo expat ctags
+    # Install dtc
+    dtc
+
+    # keep this line if you use bash
+    bashInteractive
+  ];
+
+  hardeningDisable = [ "all" ];
+
+  shellHook = ''
+    unset OBJCOPY
+  '';
+}
